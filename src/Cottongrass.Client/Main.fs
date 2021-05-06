aa3dcebfc532659ce1ca072048859b7f3ee3a7ae
AndrewIOM <AndrewIOM@users.noreply.github.com>
2021-05-06 10:38:43 +0100

    Basic structure for index pages

--- src/Cottongrass.Client/Main.fs
+++ src/Cottongrass.Client/Main.fs
@@ -13,6 +13,7 @@ module ConsultationConfig =
 
     type Consultation = YamlConfig<"templates/consultation.yml">
     type Index = YamlConfig<"templates/index.yml">
+    type SiteConfig = YamlConfig<"templates/config.yml">
 
     let loadIndex () =
         async {
@@ -38,13 +39,27 @@ module ConsultationConfig =
             return reader
         }
 
+    let loadConfig () =
+        async {
+            use client = new HttpClient(BaseAddress = Uri("http://localhost:5000"))
+            let reader = SiteConfig()
+            let! data =
+                "data/config.yml"
+                |> client.GetStringAsync
+                |> Async.AwaitTask
+            reader.LoadText data
+            return reader
+        }
+
 type Page =
     | Home
     | Consultation of shortcode: string
+    | AnswerForm of shortcode: string
 
 type Model =
     {
         page: Page
+        SiteConfig: ConsultationConfig.SiteConfig option
         Consultations: ConsultationConfig.Index.consultations_Item_Type list
         SelectedConsultation: ConsultationConfig.Consultation option
         x: string
@@ -56,6 +71,7 @@ let initModel =
         page = Home
         x = ""
         Consultations = []
+        SiteConfig = None
         SelectedConsultation = None
         Error = None
     }
@@ -63,6 +79,8 @@ let initModel =
 type Message =
     | SetPage of Page
     | Ping
+    | LoadSiteConfig
+    | LoadedSiteConfig of ConsultationConfig.SiteConfig
     | LoadIndex
     | LoadedIndex of ConsultationConfig.Index.consultations_Item_Type list
     | LoadConsultation of string
@@ -88,37 +106,122 @@ let update message model =
             Error
     | LoadedConsultation data ->
         { model with SelectedConsultation = Some data }, Cmd.none
+    | LoadSiteConfig -> 
+        model, 
+        Cmd.OfAsync.either
+            ConsultationConfig.loadConfig ()
+            (fun c -> LoadedSiteConfig c)
+            Error
+    | LoadedSiteConfig data ->
+        { model with SiteConfig = Some data }, Cmd.none
     | Error e -> { model with Error = e.ToString() |> Some }, Cmd.none
 
 let router = Router.infer SetPage (fun m -> m.page)
 
 let homeView model dispatch =
-    div [] [
-        text "Hello, world!"
+    concat [
         if model.Error.IsSome then textf "Error: %A" model.Error.Value
-        forEach model.Consultations <| fun c ->
-            div [] [
-                a [ router.HRef <| Consultation c.shortcode
-                    on.click (fun _ -> LoadConsultation c.shortcode |> dispatch) ] [ text c.title ]
+        section [ attr.``class`` "hero is-medium is-link home-hero" ] [
+            div [ attr.``class`` "hero-body" ] [
+                cond model.SiteConfig <| function
+                | Some sc ->
+                    concat [
+                        p [ attr.``class`` "title" ] [ text sc.Site.LongTitle ]
+                        p [ attr.``class`` "subtitle" ] [ text sc.Site.Subtitle ]
+                    ]
+                | None -> p [ attr.``class`` "title" ] [ text "Loading..." ]
             ]
+        ]
+        section [ attr.``class`` "section"; attr.name "Consultation list" ] [
+            div [ attr.``class`` "container" ] [
+                cond model.SiteConfig <| function
+                | Some sc -> p [] [ text sc.Site.IntroText ]
+                | None -> empty
+                h2 [ attr.``class`` "is-size-3" ] [ text "Open consultations" ]
+                forEach model.Consultations <| fun c ->
+                    div [ attr.``class`` "card" ] [
+                        header [ attr.``class`` "card-header" ] [
+                            p [ attr.``class`` "card-header-title" ] [
+                                a [ router.HRef <| Consultation c.shortcode
+                                    on.click (fun _ -> LoadConsultation c.shortcode |> dispatch) ] [ text c.title ]
+                            ]
+                        ]
+                        div [ attr.``class`` "card-content" ] [ c.description |> text ]
+                        div [ attr.``class`` "card-footer" ] [
+                            a [ router.HRef <| Consultation c.shortcode
+                                on.click (fun _ -> LoadConsultation c.shortcode |> dispatch) ] [ text "See more" ]
+                        ]
+                    ]
+            ]
+        ]
     ]
 
-let view model dispatch =
-    cond model.page <| function
-    | Home -> homeView model dispatch
-    | Consultation _ ->
-        cond model.SelectedConsultation <| function
-        | None -> text "Loading consultation..."
-        | Some active ->
-            div [] [ 
-                h2 [] [ text active.Title ]
-                p [] [ text active.Description ]
-                p [] [ textf "There are %i sections." active.Questions.Count]
+let detailView shortcode (active:ConsultationConfig.Consultation) model dispatch =
+    concat [
+        section [ attr.``class`` "hero is-primary"; attr.name "consultation-title" ] [
+            div [ attr.``class`` "hero-body" ] [
+                p [ attr.``class`` "title" ] [ text active.Title ]
             ]
+        ]
+        section [ attr.name "consultation-details" ] [
+            div [ attr.``class`` "columns" ] [
+                div [ attr.``class`` "column is-three-quarters" ] [
+                    div [ attr.``class`` "block" ] [ text active.Description ]
+                    p [] [ text active.Description ]
+                    p [] [ textf "There are %i sections." active.Questions.Count]
+                    div [ attr.``class`` "block" ] [
+                        a [ 
+                            attr.``class`` "button is-light is-primary is-medium"
+                            router.HRef <| AnswerForm shortcode ] [
+                            text "Start now"
+                        ]
+                    ]
+                ]
+                div [ attr.``class`` "column" ] [
+                    article [ attr.``class`` "message" ] [
+                        div [ attr.``class`` "message-body" ] [
+                            h2 [] [ text "Who is consulting?" ]
+                            p [] [ text "Bla bla bla "]
+                            h2 [] [ text "Where can I find more information?" ]
+                            p [] [ text "Bla bla bla..." ]
+                        ]
+                    ]
+                ]
+            ]
+        ]
+    ]
+
+let navbar model dispatch =
+    nav [ attr.``class`` "navbar"; attr.aria "label" "main navigation" ] [
+        div [ attr.``class`` "navbar-brand" ] [
+            a [ attr.``class`` "navbar-item has-text-weight-bold is-size-5"; router.HRef Home ] [
+                img [ attr.style "height:40px"; attr.src "/images/logo-small.png" ]
+                text " Cottongrass"
+            ]
+        ]
+    ]
+
+/// The answer form renders form fields from yaml in the selected
+/// language.
+let answerFormView model dispatch =
+    div [] [ text "This is an answer form!" ]
+
+
+let view model dispatch =
+    concat [
+        navbar model dispatch
+        cond model.page <| function
+        | Home -> homeView model dispatch
+        | Consultation _ ->
+            cond model.SelectedConsultation <| function
+            | None -> text "Loading consultation..."
+            | Some active -> detailView "" (* TODO *) active model dispatch
+        | AnswerForm code -> answerFormView model dispatch
+    ]
 
 type MyApp() =
     inherit ProgramComponent<Model, Message>()
 
     override this.Program =
-        Program.mkProgram (fun _ -> initModel, Cmd.ofMsg LoadIndex) update view
+        Program.mkProgram (fun _ -> initModel, Cmd.batch [Cmd.ofMsg LoadIndex; Cmd.ofMsg LoadSiteConfig ]) update view
         |> Program.withRouter router
