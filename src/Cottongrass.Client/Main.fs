6b7509e799febe5a4c71874233d1051d9bf9712a
AndrewIOM <AndrewIOM@users.noreply.github.com>
2021-05-11 14:18:16 +0100

    Display form elements

--- src/Cottongrass.Client/Main.fs
+++ src/Cottongrass.Client/Main.fs
@@ -54,43 +54,47 @@ module ConsultationConfig =
 type Page =
     | Home
     | Consultation of shortcode: string
-    | AnswerForm of shortcode: string
+    | AnswerForm of shortcode: string * section: int
+
+type DynamicQuestionAnswer =
+    | BinaryChoice of bool
+    | Text of string
+    | Choice of string
 
 type Model =
     {
         page: Page
         SiteConfig: ConsultationConfig.SiteConfig option
         Consultations: ConsultationConfig.Index.consultations_Item_Type list
-        SelectedConsultation: ConsultationConfig.Consultation option
-        x: string
+        SelectedConsultation: (ConsultationConfig.Consultation * string * int) option
+        LanguageCode: string
         Error: string option
     }
 
 let initModel =
     {
         page = Home
-        x = ""
         Consultations = []
         SiteConfig = None
         SelectedConsultation = None
+        LanguageCode = "en-GB"
         Error = None
     }
 
 type Message =
     | SetPage of Page
-    | Ping
     | LoadSiteConfig
     | LoadedSiteConfig of ConsultationConfig.SiteConfig
     | LoadIndex
     | LoadedIndex of ConsultationConfig.Index.consultations_Item_Type list
     | LoadConsultation of string
-    | LoadedConsultation of ConsultationConfig.Consultation
+    | LoadedConsultation of ConsultationConfig.Consultation * string
     | Error of exn
+    | SetDynamicAnswer of DynamicQuestionAnswer // Bind question answers from dynamic model (in a map?)
 
 let update message model =
     match message with
     | SetPage page -> { model with page = page }, Cmd.none
-    | Ping -> model, Cmd.none
     | LoadIndex -> 
         model, 
         Cmd.OfAsync.either
@@ -102,10 +106,10 @@ let update message model =
         model, 
         Cmd.OfAsync.either
             ConsultationConfig.loadConsultation shortcode
-            (fun c -> LoadedConsultation c)
+            (fun c -> LoadedConsultation (c,shortcode))
             Error
-    | LoadedConsultation data ->
-        { model with SelectedConsultation = Some data }, Cmd.none
+    | LoadedConsultation (data,sc) ->
+        { model with SelectedConsultation = Some (data,sc,1) }, Cmd.none
     | LoadSiteConfig -> 
         model, 
         Cmd.OfAsync.either
@@ -156,13 +160,17 @@ let homeView model dispatch =
         ]
     ]
 
+let heroHeading title subtitle =
+    section [ attr.``class`` "hero is-primary" ] [
+        div [ attr.``class`` "hero-body" ] [
+            p [ attr.``class`` "title" ] [ text title ]
+            if Option.isSome subtitle then p [ attr.``class`` "subtitle" ] [ text subtitle.Value ]
+        ]
+    ]
+
 let detailView shortcode (active:ConsultationConfig.Consultation) model dispatch =
     concat [
-        section [ attr.``class`` "hero is-primary"; attr.name "consultation-title" ] [
-            div [ attr.``class`` "hero-body" ] [
-                p [ attr.``class`` "title" ] [ text active.Title ]
-            ]
-        ]
+        heroHeading active.Title None
         section [ attr.name "consultation-details" ] [
             div [ attr.``class`` "columns" ] [
                 div [ attr.``class`` "column is-three-quarters" ] [
@@ -172,7 +180,7 @@ let detailView shortcode (active:ConsultationConfig.Consultation) model dispatch
                     div [ attr.``class`` "block" ] [
                         a [ 
                             attr.``class`` "button is-light is-primary is-medium"
-                            router.HRef <| AnswerForm shortcode ] [
+                            router.HRef <| AnswerForm (shortcode,1) ] [
                             text "Start now"
                         ]
                     ]
@@ -201,10 +209,211 @@ let navbar model dispatch =
         ]
     ]
 
+module Form =
+
+    let field labelText contents =
+        div [ attr.``class`` "field" ] [
+            label [ attr.``class`` "label" ] [ text labelText ]
+            contents
+        ]
+
+    let textField labelText placeholder helpText bindHole =
+        field labelText (concat [ 
+            div [ attr.``class`` "control" ] [
+            input [ attr.``class`` "input"
+                    attr.``type`` "text"
+                    attr.placeholder placeholder
+                    bindHole ]
+            ]
+            if Option.isSome helpText
+            then p [ attr.``class`` "help" ] [ text helpText.Value ]
+        ])
+
+    let optionField id labelText optionNames bindHole =
+        field labelText (concat [
+            div [ attr.``class`` "control" ] [
+                forEach optionNames <| fun opt ->
+                    label [ attr.``class`` "radio" ] [
+                        input [ attr.``type`` "radio"
+                                attr.name id
+                                bindHole ]
+                        text opt
+                    ]
+            ]
+        ])
+
+    module Parser =
+
+        type OptionField = {
+            Options: string list
+            Selected: string
+        }
+
+        type Question = {
+            Answer: DynamicQuestionAnswer
+            View: Attr -> Node
+            ChangeHandler: Attr 
+        }
+
+        type QuestionSectionBuilder = {
+            CurrentQ: Question * bool // indicates if visible
+            PreviousQs: Node
+            PreviousQuestionModel: Map<int,Question>
+        }
+
+        let lift question = { CurrentQ = question, true; PreviousQs = empty; PreviousQuestionModel = Map.empty }
+
+        let binaryChoice name question labelOne labelTwo value =
+            { View = optionField name question [ labelOne; labelTwo ]
+              Answer = BinaryChoice value
+              ChangeHandler = bind.``checked`` value ignore }
+
+        let choiceQuestion name question choices value =
+            { View = optionField name question choices
+              Answer = Choice value
+              ChangeHandler = bind.change.string value ignore }
+
+        let textQuestion label placeholder value =
+            { View = textField label placeholder None
+              Answer = Text value
+              ChangeHandler = bind.input.string value ignore }
+
+        let andDependentQuestion (nextQuestion:Question) condition (builder:QuestionSectionBuilder) =
+            let display = condition (fst builder.CurrentQ)
+            { CurrentQ = nextQuestion, display
+              PreviousQs = (fst builder.CurrentQ).View (fst builder.CurrentQ).ChangeHandler
+              PreviousQuestionModel = Map.add 1 nextQuestion builder.PreviousQuestionModel }
+
+        let compile builder =
+            concat [
+                builder.PreviousQs
+                if snd builder.CurrentQ then (fst builder.CurrentQ).View (fst builder.CurrentQ).ChangeHandler
+            ]
+
+        let andQuestion nextQuestion builder =
+            let n = builder.PreviousQuestionModel.Count + 1
+            { CurrentQ = nextQuestion, true
+              PreviousQs = compile builder
+              PreviousQuestionModel = builder.PreviousQuestionModel |> Map.add n nextQuestion }
+
+        let parseQuestion identifier langCode (q:ConsultationConfig.Consultation.Questions_Item_Type.Questions_Item_Type) =
+            let qText =
+                match q.Question |> Seq.tryFind(fun q -> q.Language = langCode) with
+                | Some q -> q.Translation
+                | None -> failwith "No translation for question found"
+            match q.Type with
+            | "binary choice" ->
+                let labels =
+                    match q.Choices |> Seq.tryFind(fun q -> q.Language = langCode) with
+                    | Some choices -> choices.Translation
+                    | None -> failwith "No translation for choices found"
+                if labels.Count <> 2 then failwith "Binary choice must have two options"
+                binaryChoice (sprintf "q-%i" identifier) qText labels.[0] labels.[1] false
+            | "choice" ->
+                let labels =
+                    match q.Choices |> Seq.tryFind(fun q -> q.Language = langCode) with
+                    | Some choices -> choices.Translation
+                    | None -> failwith "No translation for choices found"
+                choiceQuestion (sprintf "q-%i" identifier) qText labels ""
+            | "freetext"
+            | _ -> textQuestion qText "" ""
+
+        // Test using yaml:
+
+        let rec parseYaml' langCode (remainingQs:ConsultationConfig.Consultation.Questions_Item_Type.Questions_Item_Type list) (builder:QuestionSectionBuilder option) =
+            match Seq.isEmpty remainingQs with
+            | true -> 
+                if Option.isSome builder
+                then builder.Value |> compile
+                else empty
+            | false ->
+                let question = parseQuestion (remainingQs.Length) langCode (remainingQs |> Seq.head)
+                if Option.isNone builder
+                then question |> lift |> Some |> parseYaml' langCode remainingQs.Tail
+                else builder |> Option.map (andQuestion question) |> parseYaml' langCode remainingQs.Tail
+
+        // We need to connect it to a current model?
+        // Can we construct the model dynamically as a dictionary?
+        // NB: Does this run every time the view needs to render?
+        let parseYaml language section =
+            parseYaml' language section None
+            
+
+        // Use conditional elements on model properties?
+
+        // - INPUT: 
+        // What do we need to compile?
+        // - A node that contains all rendered fields
+        // - Fields have actions attached to update model values
+
+        // let formSection languageCode model =
+        //     binaryChoice "" "" "" false
+        //     |> lift
+        //     |> andDependentQuestion (textQuestion "Label" ["placeholder"] "value") ((=) true)
+        //     |> andQuestion (textQuestion "Label" ["placeholder"] "value")
+        //     |> compile
+
+
+
+
+let aboutYouSection title dispatch =
+    concat [
+        // Render some form elements
+        form [] [
+            // Form.textField "First name" "" None
+            // Form.textField "Last name" "" None
+
+            //if Option.isSome model.Organisation
+
+            // Conditional - organisation name?
+        ]
+    ]
+
+let questionSection title (section:ConsultationConfig.Consultation.Questions_Item_Type) =
+    concat [
+        Form.Parser.parseYaml "en" (section.Questions |> Seq.toList)
+    ]
+
 /// The answer form renders form fields from yaml in the selected
 /// language.
-let answerFormView model dispatch =
-    div [] [ text "This is an answer form!" ]
+let answerFormView shortcode section (model:Model) dispatch =
+    cond model.SelectedConsultation <| function
+    | None -> div [] [ text "Loading..." ]
+    | Some (con,_,_) ->
+        let currentSection = con.Questions |> Seq.tryItem (section - 2)
+        let subtitle =
+            if section = 1 then "About You" |> Some
+            else if currentSection.IsSome
+            then (currentSection.Value.Name |> Seq.find(fun (d:ConsultationConfig.Consultation.Questions_Item_Type.Name_Item_Type) -> d.Language = "en")).Translation |> Some
+            else None
+        let description =
+            if currentSection.IsSome
+            then (currentSection.Value.Description |> Seq.find(fun (d:ConsultationConfig.Consultation.Questions_Item_Type.Description_Item_Type) -> d.Language = "en")).Translation |> Some
+            else None
+        concat [
+            heroHeading con.Title subtitle
+            // Each answer form has sections of questions.
+            // Display a sidebar containing the section progress.
+            div [ attr.``class`` "container" ] [
+                div [ attr.``class`` "columns" ] [
+                    div [ attr.``class`` "column is-one-quarter" ] [
+                        ol [] [
+                            concat [
+                                li [ on.click (fun _ -> AnswerForm(shortcode,1) |> SetPage |> dispatch ) ] [ text "About you" ]
+                                forEach [1 .. con.Questions.Count] (fun i -> li [ on.click (fun _ -> AnswerForm(shortcode,i+1) |> SetPage |> dispatch ) ] [ text (con.Questions.[i-1].Name |> Seq.find(fun d -> d.Language = "en")).Translation ])
+                            ]
+                        ]
+                    ]
+                    div [ attr.``class`` "column" ] [
+                        if description.IsSome then p [] [ text description.Value ]
+                        if section = 1 
+                        then aboutYouSection con.Title dispatch
+                        else con.Questions |> Seq.tryItem (section - 2) |> (fun o -> if o.IsSome then questionSection con.Title o.Value else text "Error")
+                        button [ attr.``class`` "button"; on.click (fun _ -> AnswerForm(shortcode,section+1) |> SetPage |> dispatch ) ] [ text "Next" ]
+                    ]
+                ]
+            ]
+        ]
 
 
 let view model dispatch =
@@ -215,8 +424,8 @@ let view model dispatch =
         | Consultation _ ->
             cond model.SelectedConsultation <| function
             | None -> text "Loading consultation..."
-            | Some active -> detailView "" (* TODO *) active model dispatch
-        | AnswerForm code -> answerFormView model dispatch
+            | Some (active,sc,_) -> detailView sc active model dispatch
+        | AnswerForm (code,section) -> answerFormView code section model dispatch
     ]
 
 type MyApp() =
